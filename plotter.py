388e007ef37e145cdd000041163f8ee9f6e45ef2
jj <jeyavinoth@gmail.com>
2019-04-08 15:56:25 -0400

    testing out the algorithm with U10

--- plotter.py
+++ plotter.py
@@ -12,8 +12,11 @@ def plot(lonGrid, latGrid, data, show=True):
   urlon = np.nanmax(lonGrid)
 
   m = Basemap(projection='cyl', urcrnrlat=urlat, urcrnrlon=urlon, llcrnrlat=lllat, llcrnrlon=lllon)
-  m.drawcoastlines()
-  m.pcolormesh(lonGrid, latGrid, data, cmap='jet')
+  m.drawcoastlines(linewidth=.5)
+  cnt = m.contourf(lonGrid, latGrid, data, cmap='jet')
+  # for c in cnt.collections:
+    # c.set_edgecolor('k')
+    # c.set_linewidth(0.01)
   m.colorbar()
   m.drawparallels(np.arange(-90, 90, 25), labels=[True, False, False, False])
   m.drawmeridians(np.arange(-180, 180, 50), labels=[False, False, False, True])
